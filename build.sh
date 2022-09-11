abd5a3f51f2ddb671200a3aa8c1428c8295e1600
y011d4 <yoiida333@gmail.com>
2022-09-11 14:59:01 +0900

    build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 for VERSION in 2.31 2.32 2.33 2.34 2.35
 do
-    docker build --build-arg GLIBC_VERSION=${VERSION} -t glibc_builder64:${VERSION} ./glibc_builder64
-    docker build --build-arg GLIBC_VERSION=${VERSION} -t glibc_builder32:${VERSION} ./glibc_builder32
+    docker build --build-arg GLIBC_VERSION=${VERSION} -t y011d4/glibc_builder64:${VERSION} ./glibc_builder64
+    docker build --build-arg GLIBC_VERSION=${VERSION} -t y011d4/glibc_builder32:${VERSION} ./glibc_builder32
 done
 docker build -t y011d4/pwndocker .
