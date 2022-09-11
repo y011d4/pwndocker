7e2a435661b879cf5748ff7e66cddc326d876d11
y011d4 <yoiida333@gmail.com>
2022-09-11 15:00:55 +0900

    push

--- build_glibc.sh
+++ build_glibc.sh
@@ -1,5 +1,7 @@
 for VERSION in 2.31 2.32 2.33 2.34 2.35
 do
     docker build --build-arg GLIBC_VERSION=${VERSION} -t y011d4/glibc_builder64:${VERSION} ./glibc_builder64
+    docker push y011d4/glibc_builder64:${VERSION}
     docker build --build-arg GLIBC_VERSION=${VERSION} -t y011d4/glibc_builder32:${VERSION} ./glibc_builder32
+    docker push y011d4/glibc_builder32:${VERSION}
 done
